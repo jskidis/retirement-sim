d58c579d03d3b26130f5867a0bb3c9840bc5c6eb
jskidis <bi8b0AT1718>
2024-02-22 21:19:24 -0600

    Added dividend and expense ration to Portfolio ROR Providers

--- src/main/kotlin/asset/LazyPortfolioLoader.kt
+++ src/main/kotlin/asset/LazyPortfolioLoader.kt
@@ -9,9 +9,11 @@ object LazyPortfolioLoader {
             it[0].trim() to LazyPortfolioRORProvider(
                 mean = it[1].toDouble(),
                 stdDev = it[2].toDouble(),
-                stockPct = it[3].toDouble(),
-                bondPct = it[4].toDouble(),
-                ulcerIndex = it[5].toDouble())
+                divid = it[3].toDouble(),
+                expRatio = it[4].toDouble(),
+                stockPct = it[5].toDouble(),
+                bondPct = it[6].toDouble(),
+                ulcerIndex = it[7].toDouble())
         }
 
     fun loadPortfolios(resourcePath: String = "tables/lazy-portfolios.csv")
