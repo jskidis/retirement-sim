680a212f51936c9f7fcbc10f2baf8955250622d5
jskidis <jason@skidis.com>
2024-03-21 08:58:15 -0500

    Calcuate CarryOver (2nd pass) taxable amounts and incorporated those into next year tax calculations

--- src/test/kotlin/tax/TaxesProcessorTest.kt
+++ src/test/kotlin/tax/TaxesProcessorTest.kt
@@ -1,10 +1,14 @@
 package tax
 
+import Year
+import asset.AssetChange
+import asset.assetConfigProgressFixture
 import asset.assetRecFixture
 import config.configFixture
 import expense.expenseRecFixture
 import income.incomeRecFixture
 import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 import yearlyDetailFixture
 
@@ -27,6 +31,9 @@ class TaxesProcessorTest : ShouldSpec({
         gains = 500.0, taxProfile = NonWageTaxableProfile()
     )
 
+    val noCarryOver = ArrayList<TaxableAmounts>()
+    val carryOver = TaxableAmounts(person = "P", fed = 1000.0, fedLTG = 100.0, state = 1100.0)
+
     val fedTaxCalc = FixedRateTaxCalc(.10)
     val fedLTGTaxCalc = FixedRateTaxCalc(.05)
     val stateTaxCalc = FixedRateTaxCalc(.04)
@@ -42,7 +49,7 @@ class TaxesProcessorTest : ShouldSpec({
             incomes = listOf(wageInc)
         )
 
-        val result = TaxesProcessor.processTaxes(currYear, config)
+        val result = TaxesProcessor.processTaxes(currYear, noCarryOver, config)
         result.fed.shouldBe(wageInc.amount * fedTaxCalc.pct)
         result.state.shouldBe(wageInc.amount * stateTaxCalc.pct)
         result.socSec.shouldBe(wageInc.amount * socSecTaxCalc.pct)
@@ -54,7 +61,7 @@ class TaxesProcessorTest : ShouldSpec({
             incomes = listOf(wageInc, fedOnlyInc)
         )
 
-        val result = TaxesProcessor.processTaxes(currYear, config)
+        val result = TaxesProcessor.processTaxes(currYear, noCarryOver, config)
         result.fed.shouldBe((wageInc.amount + fedOnlyInc.amount) * fedTaxCalc.pct)
         result.state.shouldBe((wageInc.amount) * stateTaxCalc.pct)
         result.socSec.shouldBe(wageInc.amount * socSecTaxCalc.pct)
@@ -67,7 +74,7 @@ class TaxesProcessorTest : ShouldSpec({
             expenses = listOf(nonDeductExp, decductExp)
         )
 
-        val result = TaxesProcessor.processTaxes(currYear, config)
+        val result = TaxesProcessor.processTaxes(currYear, noCarryOver, config)
         result.fed.shouldBe((wageInc.amount + fedOnlyInc.amount - decductExp.amount) * fedTaxCalc.pct)
         result.state.shouldBe((wageInc.amount - decductExp.amount) * stateTaxCalc.pct)
         result.socSec.shouldBe(wageInc.amount * socSecTaxCalc.pct)
@@ -81,10 +88,64 @@ class TaxesProcessorTest : ShouldSpec({
             assets = listOf(assetRec)
         )
 
-        val result = TaxesProcessor.processTaxes(currYear, config)
+        val result = TaxesProcessor.processTaxes(currYear, noCarryOver, config)
         result.fed.shouldBe((wageInc.amount + fedOnlyInc.amount + assetRec.totalGains() - decductExp.amount) * fedTaxCalc.pct)
         result.state.shouldBe((wageInc.amount + assetRec.totalGains() - decductExp.amount) * stateTaxCalc.pct)
         result.socSec.shouldBe(wageInc.amount * socSecTaxCalc.pct)
         result.medicare.shouldBe(wageInc.amount * medicareTaxCalc.pct)
     }
+
+    should("processTaxes includes carryOver taxable in calcuations") {
+        val currYear = yearlyDetailFixture().copy(
+            incomes = listOf(wageInc)
+        )
+
+
+        val result = TaxesProcessor.processTaxes(currYear, listOf(carryOver), config)
+        result.fed.shouldBe((
+            wageInc.amount + carryOver.fed) * fedTaxCalc.pct +
+            (carryOver.fedLTG * fedLTGTaxCalc.pct))
+        result.state.shouldBe((wageInc.amount + carryOver.state) * stateTaxCalc.pct)
+        result.socSec.shouldBe(wageInc.amount * socSecTaxCalc.pct)
+        result.medicare.shouldBe(wageInc.amount * medicareTaxCalc.pct)
+
+    }
+
+    should("calculate carryOver taxable amounts") {
+        val year: Year = 2020
+
+        val assetConfig1 = assetConfigProgressFixture(name = "Asset 1")
+        val taxable1 = TaxableAmounts(person = "Person", fed = 1000.0, state = 1000.0)
+        val assetRec1 = assetRecFixture(year = year, assetConfig = assetConfig1.config)
+        val tribution1 = AssetChange(name = "W", amount = -1000.0, taxable1, isCarryOver = true)
+        assetRec1.tributions.add(tribution1)
+
+        val assetConfig2 = assetConfigProgressFixture(name = "Asset 2")
+        val taxable2 = TaxableAmounts(person = "Person", fed = 2000.0, state = 2000.0)
+        val assetRec2 = assetRecFixture(year = year, assetConfig = assetConfig2.config)
+        val tribution2 = AssetChange(name = "W", amount = -2000.0, taxable2, isCarryOver = true)
+        assetRec2.tributions.add(tribution2)
+
+        val assetConfig3 = assetConfigProgressFixture(name = "Asset 3")
+        val taxable3 = TaxableAmounts(person = "Person", fed = -3000.0, state = -3000.0)
+        val assetRec3 = assetRecFixture(year = year, assetConfig = assetConfig3.config)
+        val tribution3 = AssetChange(name = "D", amount = 3000.0, taxable3, isCarryOver = false)
+        assetRec3.tributions.add(tribution3)
+
+        val assetConfig4 = assetConfigProgressFixture(name = "Asset 4")
+        val assetRec4 = assetRecFixture(year = year, assetConfig = assetConfig4.config)
+        val tribution4 = AssetChange(name = "D", amount = 100.0, taxable = null, isCarryOver = true)
+        assetRec4.tributions.add(tribution4)
+
+        val currYear = yearlyDetailFixture(year, assets = listOf(
+            assetRec1, assetRec2, assetRec3, assetRec4
+        ))
+
+        val result = TaxesProcessor.carryOverTaxable(currYear)
+        result.size.shouldBe(2)
+        result.shouldContain(taxable1)
+        result.shouldContain(taxable2)
+    }
 })
+
+
