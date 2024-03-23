a5cbc06cb51ec2a67555c0099cd78b9883978bd5
jskidis <bi8b0AT1718>
2024-03-22 21:51:50 -0500

    Simplification of PrevRecProviderProgression

--- src/main/kotlin/progression/PrevRecProviderProgression.kt
+++ src/main/kotlin/progression/PrevRecProviderProgression.kt
@@ -2,15 +2,13 @@ package progression
 
 import YearlyDetail
 
-interface PrevRecProvider<T> {
+interface PrevRecProviderProgression<T> :
+    Progression<T> {
+
     fun initialValue(): T
     fun previousValue(prevYear: YearlyDetail): T
-}
-
-interface PrevRecProviderProgression<T> :
-    Progression<T>, PrevRecProvider<T> {
-
     fun next(prevVal: T): T
+
     override fun determineNext(prevYear: YearlyDetail?): T =
         if (prevYear == null) initialValue()
         else next(previousValue(prevYear))
