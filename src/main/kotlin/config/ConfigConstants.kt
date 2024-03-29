8970983bc62e149deede541d6441012d7ca0cac1
jskidis <jason@skidis.com>
2024-03-28 22:07:12 -0500

    Better solution for constants

--- src/main/kotlin/config/ConfigConstants.kt
+++
@@ -1,9 +0,0 @@
-package config
-
-import Amount
-
-object ConfigConstants {
-    val socSecIncomeCapBase: Amount = 168600.0
-    var marketPlaceBasePrem: Amount = 5500.0
-    var baseMedicarePrem: Amount = 2250.0
-}
