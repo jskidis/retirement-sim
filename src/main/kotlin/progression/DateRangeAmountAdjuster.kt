c6ea16a1ef71c2fc1bcbb97d24d198599b5428ed
jskidis <jason@skidis.com>
2024-06-22 11:29:46 -0500

    Fixed bug in DateRangeAmountAdjuster that didn't take into the previous year may have been a partial year

--- src/main/kotlin/progression/DateRangeAmountAdjuster.kt
+++ src/main/kotlin/progression/DateRangeAmountAdjuster.kt
@@ -4,10 +4,18 @@ import Amount
 import YearlyDetail
 import util.DateRange
 
-open class DateRangeAmountAdjuster(val dateRange: DateRange): AmountAdjusterWithGapFiller {
-    override fun adjustAmount(value: Amount, prevYear: YearlyDetail): Amount =
-        value * dateRange.pctInYear(prevYear.year +1).value
+open class DateRangeAmountAdjuster(
+    val dateRange: DateRange,
+) : AmountAdjusterWithGapFiller {
+
+    override fun adjustAmount(value: Amount, prevYear: YearlyDetail): Amount {
+        val pctInPrevYear = dateRange.pctInYear(prevYear.year).value
+        val pctInCurrYear = dateRange.pctInYear(prevYear.year + 1).value
+        return value * pctInCurrYear /
+            if (pctInPrevYear == 0.0) 1.0
+            else pctInPrevYear
+    }
 
     override fun adjustGapFillValue(value: Amount, prevYear: YearlyDetail): Amount =
-        adjustAmount(value, prevYear)
+        value * dateRange.pctInYear(prevYear.year + 1).value
 }
--- src/test/kotlin/progression/DateRangeAmountAdjusterTest.kt
+++ src/test/kotlin/progression/DateRangeAmountAdjusterTest.kt
@@ -4,29 +4,51 @@ import YearMonth
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 import util.DateRange
+import util.currentDate
 import yearlyDetailFixture
 
 class DateRangeAmountAdjusterTest : ShouldSpec({
 
-    val dateRange = DateRange(YearMonth(2020, 0), YearMonth(2025, 6))
+    val currentYear = currentDate.year
+    val rangeEndsThisYear = DateRange(YearMonth(2020, 0), YearMonth(currentYear, 6))
+    val rangeStartsThisYear = DateRange(YearMonth(currentYear, 6), YearMonth(2100, 12))
 
-    // Adjuster increments prevYear's year value to determine if current year is in date range
-    // so a previous year of 2024, would be in the partial range for a date range that ended during 2025
-    val fullyInRange = yearlyDetailFixture(2023)
-    val partialInRange = yearlyDetailFixture(2024)
-    val outOfRange = yearlyDetailFixture(2025)
     val unadjustedValue = 1000.0
 
-    val adjuster = DateRangeAmountAdjuster(dateRange)
+    should("adjustAmount when range ends this year") {
+        val adjuster = DateRangeAmountAdjuster(rangeEndsThisYear)
 
-    should("adjustAmount") {
-        adjuster.adjustAmount(unadjustedValue, fullyInRange).shouldBe(unadjustedValue)
-        adjuster.adjustAmount(unadjustedValue, partialInRange).shouldBe(unadjustedValue * 0.5)
-        adjuster.adjustAmount(unadjustedValue, outOfRange).shouldBe(0)
+        adjuster.adjustAmount(unadjustedValue, prevYear = yearlyDetailFixture(currentYear - 2))
+            .shouldBe(unadjustedValue) // curr year is last year
+
+        adjuster.adjustAmount(unadjustedValue, prevYear = yearlyDetailFixture(currentYear - 1))
+            .shouldBe(unadjustedValue * 0.5) // curr year is this year
+
+        adjuster.adjustAmount(unadjustedValue * 0.5, prevYear = yearlyDetailFixture(currentYear))
+            .shouldBe(0.0) // curr year is next year
     }
     should("adjustGapFillValue") {
-        adjuster.adjustGapFillValue(unadjustedValue, fullyInRange).shouldBe(unadjustedValue)
-        adjuster.adjustGapFillValue(unadjustedValue, partialInRange).shouldBe(unadjustedValue * 0.5)
-        adjuster.adjustGapFillValue(unadjustedValue, outOfRange).shouldBe(0)
+        val adjuster = DateRangeAmountAdjuster(rangeEndsThisYear)
+
+        adjuster.adjustGapFillValue(unadjustedValue,
+            prevYear = yearlyDetailFixture(currentYear - 2)).shouldBe(unadjustedValue)
+
+        adjuster.adjustGapFillValue(unadjustedValue,
+            prevYear = yearlyDetailFixture(currentYear - 1)).shouldBe(unadjustedValue * 0.5)
+
+        adjuster.adjustGapFillValue(unadjustedValue * 0.5,
+            prevYear = yearlyDetailFixture(currentYear)).shouldBe(0.0)
+    }
+    should("account for previous year being partial") {
+        val adjuster = DateRangeAmountAdjuster(rangeStartsThisYear)
+
+        adjuster.adjustAmount(unadjustedValue, prevYear = yearlyDetailFixture(currentYear - 2))
+            .shouldBe(0.0)
+
+        adjuster.adjustAmount(unadjustedValue, prevYear = yearlyDetailFixture(currentYear - 1))
+            .shouldBe(unadjustedValue * 0.5)
+
+        adjuster.adjustAmount(unadjustedValue * 0.5, prevYear = yearlyDetailFixture(currentYear))
+            .shouldBe(unadjustedValue)
     }
 })
